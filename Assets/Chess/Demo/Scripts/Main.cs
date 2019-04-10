dd498fd1f746b95419f61f65f3d0ff425834e35d
pedjamitrovic <pre11mit@yahoo.com>
2019-04-10 15:16:03 +0200

    Initial commit

---
+++ Assets/Chess/Demo/Scripts/Main.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+	public ChessUiEngine uiEngine;
+	public Text cell;
+	public Transform brightSquare;
+	// Use this for initialization
+	void Start () {
+		uiEngine.SetupPieces ();
+	}
+
+	void FixedUpdate () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+		int cellNumber = uiEngine.RaycastCell (ray);
+		if (!IsValidCell(cellNumber)) {
+			return;
+		}
+		cell.text = ChessUiEngine.GetCellString(cellNumber);
+		PlaceBrightSquare (cellNumber);
+	}
+
+	void PlaceBrightSquare (int cellNumber)
+	{
+		brightSquare.position = ChessUiEngine.ToWorldPoint (cellNumber);
+	}
+
+	bool IsValidCell (int cellNumber)
+	{
+		return cellNumber >= 0 && cellNumber < 64;
+	}
+}
