c7d7b5942ce40cfeef28542fc7cf8f4983c616f5
pedjamitrovic <pre11mit@yahoo.com>
2019-07-13 19:11:16 +0200

    HashGenerator implemented

--- Assets/Project/ChessEngine/Board.cs
+++ Assets/Project/ChessEngine/Board.cs
@@ -7,18 +7,19 @@ namespace Assets.Project.ChessEngine
 {
     public class Board
     {
-        public byte[] Pieces { get; set; }
+        public SquareContent[] Pieces { get; set; }
         public Bitboard[] Pawns { get; set; }
-        public byte[] Kings { get; set; }
+        public Square[] Kings { get; set; }
         public byte[] PieceCount { get; set; }
         public byte[] BigPiecesCount { get; set; }
         public byte[] MajorPiecesCount { get; set; }
         public byte[] MinorPiecesCount { get; set; }
         public byte[,] PieceList { get; set; }
 
-        public byte OnTurn { get; set; }
+        public Side OnTurn { get; set; }
         public byte FiftyMove { get; set; }
-        public int EnPassant { get; set; }
+        public Square EnPassant { get; set; }
+        public int CastlePerm { get; set; }
 
         public int HistoryPly { get; set; } // number of halfmoves in the game so far
         public int Ply { get; set; } // number of halfmoves in the move search so far
@@ -26,43 +27,60 @@ namespace Assets.Project.ChessEngine
 
         public Board()
         {
-            Pieces = new byte[BoardSquaresNumber];
-            Pawns = new Bitboard[PawnsRepresentationNumber];
-            for (int i = 0; i < PawnsRepresentationNumber; i++) Pawns[i] = new Bitboard();
-            Kings = new byte[KingsRepresentationNumber];
+            Pieces = new SquareContent[BoardSquaresNumber];
+            for (int i = 0; i < BoardSquaresNumber; ++i) Pieces[i] = SquareContent.OffLimits;
+            for (int i = 0; i < 64; ++i) Pieces[SqIndexes64To120[i]] = SquareContent.None;
+
+            Kings = new Square[KingsRepresentationNumber];
+            Kings[(byte)Side.White] = Kings[(byte)Side.Black] = Square.None;
+
             PieceCount = new byte[PieceTypesCount];
+            for (int i = 0; i < PieceTypesCount; ++i)
+            {
+                PieceCount[i] = 0;
+            }
+
+            Pawns = new Bitboard[PawnsRepresentationNumber];
             BigPiecesCount = new byte[BigPiecesRepresentationNumber];
             MajorPiecesCount = new byte[MajorPiecesRepresentationNumber];
             MinorPiecesCount = new byte[MinorPiecesRepresentationNumber];
-            PieceList = new byte[PieceTypesCount, MaxCountOfPieceType];
+            for (int i = 0; i < 3; ++i)
+            {
+                Pawns[i] = new Bitboard();
+                BigPiecesCount[i] = 0;
+                MajorPiecesCount[i] = 0;
+                MinorPiecesCount[i] = 0;
+            }
+
+            OnTurn = Side.Both;
             FiftyMove = 0;
+            EnPassant = Square.None;
+            CastlePerm = 0;
+
+            HistoryPly = 0;
+            Ply = 0;
+
+            StateKey = 0;
         }
 
-        /* 120 square board representation style */
-        public static readonly int BoardSquaresNumber = 120;
-        /* keeping track of white, black and both pawn positions in 64bit array */
-        public static readonly int PawnsRepresentationNumber = 3;
-        /* keeping track of both white and black king position */
-        public static readonly int KingsRepresentationNumber = 2;
-        /* keeping track of both white and black piece type count */
-        public static readonly int PieceTypesCount = 13;
-        /* keeping track of both white and black piece count (excluding pawns) */
-        public static readonly int BigPiecesRepresentationNumber = 3;
-        /* keeping track of both white and black queens and rooks count */
-        public static readonly int MajorPiecesRepresentationNumber = 3;
-        /* keeping track of both white and black knights and bishops count */
-        public static readonly int MinorPiecesRepresentationNumber = 3;
-        /* 8 pawns can upgrade to same figure, plus max 2 same figures on board */
-        public static readonly int MaxCountOfPieceType = 10;
+        public static readonly int BoardSquaresNumber = 120; // 120 square board representation style
+        public static readonly int PawnsRepresentationNumber = 3; // keeping track of white, black and both pawn positions in 64bit array
+        public static readonly int KingsRepresentationNumber = 2; // keeping track of both white and black king position
+        public static readonly int PieceTypesCount = 13; // keeping track of both white and black piece type count
+        public static readonly int BigPiecesRepresentationNumber = 3; // keeping track of both white and black piece count (excluding pawns)
+        public static readonly int MajorPiecesRepresentationNumber = 3; // keeping track of both white and black queens and rooks count
+        public static readonly int MinorPiecesRepresentationNumber = 3; // keeping track of both white and black knights and bishops count
+        public static readonly int MaxCountOfPieceType = 10; // 8 pawns can upgrade to same figure, plus max 2 same figures on board
 
         public static readonly byte[] SqIndexes120To64;
         public static readonly byte[] SqIndexes64To120;
+
         static Board()
         {
             SqIndexes120To64 = new byte[120];
             SqIndexes64To120 = new byte[64];
-            for(int i = 0; i < 120; i++) SqIndexes120To64[i] = 64;
-            for(int i = 0; i < 64; i++) SqIndexes64To120[i] = 120;
+            for (int i = 0; i < 120; i++) SqIndexes120To64[i] = (byte)SquareContent.OffLimits;
+            for (int i = 0; i < 64; i++) SqIndexes64To120[i] = (byte)SquareContent.OffLimits;
 
             byte currSq, sq64 = 0;
             for (Rank r = Rank.Rank1; r <= Rank.Rank8; ++r)
@@ -75,8 +93,10 @@ namespace Assets.Project.ChessEngine
                 }
             }
         }
-        public static byte ConvertToSq120(File f, Rank r) {
-            return (byte) ((int)r * 10 + (int)f + 21);
+
+        public static byte ConvertToSq120(File f, Rank r)
+        {
+            return (byte)((int)r * 10 + (int)f + 21);
         }
     }
 }
---
+++ Assets/Project/ChessEngine/HashGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Project.ChessEngine
+{
+    public static class HashGenerator
+    {
+        private static ulong[,] pieceKeys; // 13x120
+        private static ulong sideKey;
+        private static ulong[] castleKeys; // 16 (4 bit representation 0-15 values)
+        private static Random rnd = new Random();
+
+        static HashGenerator()
+        {
+            pieceKeys = new ulong[Board.PieceTypesCount, Board.BoardSquaresNumber];
+            castleKeys = new ulong[16];
+            for (int i = 0; i < Board.PieceTypesCount; ++i)
+            {
+                for (int j = 0; j < 120; ++j)
+                {
+                    pieceKeys[i, j] = Get64BitRandom();
+                }
+            }
+            sideKey = Get64BitRandom();
+            for (int i = 0; i < 16; ++i)
+            {
+                castleKeys[i] = Get64BitRandom();
+            }
+        }
+
+        public static ulong CalculateStateKey(Board board)
+        {
+            ulong finalKey = 0;
+            SquareContent piece;
+
+            for (int sq = 0; sq < Board.BoardSquaresNumber; ++sq)
+            {
+                piece = board.Pieces[sq];
+                if (piece != SquareContent.None && piece != SquareContent.OffLimits)
+                {
+                    finalKey ^= pieceKeys[(byte)piece, sq];
+                }
+            }
+
+            if (board.OnTurn == Side.White) finalKey ^= sideKey;
+
+            if (board.EnPassant != Square.None) finalKey ^= pieceKeys[0, (byte)board.EnPassant];
+
+            finalKey ^= castleKeys[board.CastlePerm];
+
+            return finalKey;
+        }
+
+        private static ulong Get64BitRandom(ulong minValue = ulong.MinValue, ulong maxValue = ulong.MaxValue)
+        {
+            byte[] buffer = new byte[sizeof(ulong)];
+            rnd.NextBytes(buffer);
+            return BitConverter.ToUInt64(buffer, 0) % (maxValue - minValue + 1) + minValue;
+        }
+    }
+}
